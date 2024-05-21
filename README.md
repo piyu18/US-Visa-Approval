f306fe11f429d08cc9a1ea5646e74bed24d692e3
priya <priya1803singh@gmail.com>
2024-05-21 22:31:04 +0530

    folder structure created

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # US-Visa-Approval
+
+
+## Create a new environment
+```bash
+conda create -p venv python=3.8 -y
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='US_Visa',
+    version='0.1',
+    author='Priya',
+    author_email='priya1803singh@gmail.com',
+    packages=find_packages()
+)
